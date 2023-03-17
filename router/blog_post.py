0ef721c88cb1b0cdba2664356873c7bbd228d6d6
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2023-03-17 22:07:27 +0100

    more on query parameters

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,7 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter(prefix='/blog', tags=['blog'])
 
-@router.post('/')
-def create_blog():
-    pass
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    number_comment: int
+    publish: Optional[bool]
+
+
+@router.post('/new/{id}')
+def create_blog(blog:BlogModel, id:int, version:int= 2):
+    return {
+        "id": id,
+        "version": version,
+        "data": blog
+    }
+
+@router.post('/new/{id}/comment')
+def create_comment(blog:BlogModel,
+ id=int, comment_id:int = Query(None,
+ title="Id of the comment",
+ description="Some random string",
+ alias="commentId")):
+    return{
+        "data": blog,
+        "id": id,
+        "comment_id" : comment_id
+    }
+
+
+
+
+
