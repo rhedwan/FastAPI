3256c12ce800be16255828e561b602e561bfd41a
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2023-03-18 23:23:03 +0100

    remove password in the response

--- router/user.py
+++ router/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter , Depends
-from schemas import UserBase
+from schemas import UserBase, UserDisplay
 from sqlalchemy.orm.session import Session
 from db.database import get_db
 from db import db_user
@@ -8,6 +8,6 @@ router = APIRouter(
     prefix="/user", tags=["user"]
 )
 
-@router.post("/")
+@router.post("/", response_model=UserDisplay)
 def create_user(request: UserBase, db:Session = Depends(get_db)):
     return db_user.create_user(db, request)
